449dd38474ece67603f88f3e02e3a4b5daee16b3
Nick <Nicholashathaway@gmail.com>
2017-12-15 12:26:20 -0700

    image drag and drop with preview

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { image: {} };
+  }
+
+  onDrop(acceptedFiles, rejectedFiles) {
+    console.log('Accepted files: ', acceptedFiles[0].name);
+    let image = this.state.image;
+    image = acceptedFiles[0];
+    this.setState({ image }); 
+  }
+
   render() {
+    console.log(this.state);
     return (
-      <div className="app-wrapper">
+      <div className="App">
+        <Dropzone onDrop={(files) => this.onDrop(files)}>
+          <div>Try dropping some files here, or click to select files to upload.</div>
+        </Dropzone>
+        <img src={ this.state.image.preview }/>
       </div>
-    );
+      )
   }
 }
 
