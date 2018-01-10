46f8aad7dfae55b70d3013e8a44329728e7e3849
Nick <Nicholashathaway@gmail.com>
2018-01-10 15:20:59 -0500

    map over table data to check values from cropper dimensions

--- src/components/CompatibilityTable.js
+++ src/components/CompatibilityTable.js
@@ -1,82 +1,45 @@
 import React, { Component } from 'react';
 import { Icon, Label, Menu, Table } from 'semantic-ui-react';
 
+import tableData from './tableData';
+
 export default class CompatibilityTable extends Component {
+  constructor() {
+    super();
+    this.state = { tableData }
+  }
+
   render() {
+
+    const tableData = this.state.tableData.map(site => (
+      <Table.Row>
+        <Table.Cell>{ site.website }</Table.Cell>
+        <Table.Cell>{ site.fileTypes }</Table.Cell>
+        <Table.Cell>{ site.fileSize }</Table.Cell>
+        <Table.Cell>{ site.width }px</Table.Cell>
+        <Table.Cell>{ site.height }px</Table.Cell>
+      </Table.Row>
+    ))
+
     return (
       <div className='table-wrapper'>
         <p className='dimensions'>{ `Image Width: ${this.props.cropperSize.width}, Image Height: ${this.props.cropperSize.height}` }</p>
         <Table celled>
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell>Websites</Table.HeaderCell>
-              <Table.HeaderCell>File Type</Table.HeaderCell>
-              <Table.HeaderCell>File Size</Table.HeaderCell>
-              <Table.HeaderCell>Min-Width</Table.HeaderCell>
-              <Table.HeaderCell>Min-Height</Table.HeaderCell>
-              <Table.HeaderCell>Max-Width</Table.HeaderCell>
-              <Table.HeaderCell>Max-Height</Table.HeaderCell>
+              <Table.HeaderCell>Website</Table.HeaderCell>
+              <Table.HeaderCell>File Types</Table.HeaderCell>
+              <Table.HeaderCell>Max. File Size</Table.HeaderCell>
+              <Table.HeaderCell>Width</Table.HeaderCell>
+              <Table.HeaderCell>Height</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            <Table.Row>
-              <Table.Cell>Facebook</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell negative>Cell</Table.Cell>
-              <Table.Cell negative>Cell</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>YouTube</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>Instagram</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>Snapchat</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell negative>Cell</Table.Cell>
-              <Table.Cell negative>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell negative>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>Linkedin</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>Tumblr</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell positive>Cell</Table.Cell>
-              <Table.Cell negative>Cell</Table.Cell>
-              <Table.Cell negative>Cell</Table.Cell>
-              <Table.Cell negative>Cell</Table.Cell>
-              <Table.Cell negative>Cell</Table.Cell>
-            </Table.Row>
+            { tableData }
           </Table.Body>
           <Table.Footer>
             <Table.Row>
-              <Table.HeaderCell colSpan='7'>
+              <Table.HeaderCell colSpan='5'>
                 <Menu floated='right' pagination>
                   <Menu.Item as='a'>Profile</Menu.Item>
                   <Menu.Item as='a'>Cover</Menu.Item>
---
+++ src/components/tableData.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    website: 'Facebook',
+    fileTypes: 'JPEG, GIF, PNG',
+    fileSize: 'N/A',
+    width: 170,
+    height: 170
+  },
+  {
+    website: 'YouTube',
+    fileTypes: 'N/A',
+    fileSize: 'N/A',
+    width: null,
+    height: null
+  },
+  {
+    website: 'Twitter',
+    fileTypes: 'JPEG, GIF, PNG',
+    fileSize: '2MB',
+    width: 400,
+    height: 400
+  },
+  {
+    website: 'Instagram',
+    fileTypes: 'N/A',
+    fileSize: 'N/A',
+    width: null,
+    height: null
+  },
+  {
+    website: 'Linkedin',
+    fileTypes: 'N/A',
+    fileSize: 'N/A',
+    width: null,
+    height: null
+  }
+];
